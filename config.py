0f6356cc6c7806ce7360a341c082c5d98c73bcc1
v70918020-ux <v70918020@gmail.com>
2026-01-06 10:03:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ OWNER_ID = int(getenv("OWNER_ID", "7491374623"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7491374623").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002226641720"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002226641720")) "22746239"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002955601418"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002955601418")) "22746239"))
 API_HASH = environ.get("API_HASH", "4fd02c02368941a03d48ecab3a3b4a31")
 
 """
@@ -37,9 +37,9 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7491374623"))
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7491374623").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002674886829"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002955601418"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://mongodburl9721:rfNOuo4c1OSTZ7cs@cluster0.edezeww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002674886829"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002955601418"))
 
